5d049c2fa5022fde9cb1524c78de9b4e16109461
Pradeep Mali <malipradeep591@gmail.com>
2026-09-05 19:54:02 +0530

    Initial project setup

---
+++ src/main/java/com/pradeepmali591/seed_ai/SeedAiApplication.java
@@ -0,0 +1,13 @@
+package com.pradeepmali591.seed_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeepmali591/seed_ai/SeedAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeepmali591.seed_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
